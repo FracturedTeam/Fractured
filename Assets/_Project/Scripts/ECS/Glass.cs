b81064ea93f487f4005e64fbf861d346ca878789
IdkHowButHereIAm <114149335+IdkHowButHereIAm@users.noreply.github.com>
2025-10-31 14:08:38 +0100

    Update Glass.cs

--- Assets/_Project/Scripts/ECS/Glass.cs
+++ Assets/_Project/Scripts/ECS/Glass.cs
@@ -41,7 +41,8 @@ namespace _Project.Scripts.ECS {
         private void Update() {
             if(isHeld && (GameInitializer.Instance.InEditableArea() || canEditAnywhere))
             {
-                transform.position = Mouse.current.position.ReadValue();
+                transform.position = new Vector2(Math.Clamp(Mouse.current.position.ReadValue().x, 0  + shardSprite.minWidth,  Screen.width - shardSprite.minWidth),
+                    Mathf.Clamp(Mouse.current.position.ReadValue().y,  0 + shardSprite.sprite.textureRect.height/4 ,  Screen.height  - shardSprite.sprite.textureRect.height/4));
             }
         }
         
