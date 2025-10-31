633e89e982d269daf0026ce9310e5a2a607d113b
IdkHowButHereIAm <114149335+IdkHowButHereIAm@users.noreply.github.com>
2025-10-31 14:46:48 +0100

    screen size debug

--- Assets/_Project/Scripts/ECS/Glass.cs
+++ Assets/_Project/Scripts/ECS/Glass.cs
@@ -41,8 +41,8 @@ namespace _Project.Scripts.ECS {
         private void Update() {
             if(isHeld && (GameInitializer.Instance.InEditableArea() || canEditAnywhere))
             {
-                transform.position = new Vector2(Math.Clamp(Mouse.current.position.ReadValue().x, 0  + shardSprite.rectTransform.sizeDelta.x /4,  Screen.width - shardSprite.rectTransform.sizeDelta.x /4),
-                    Mathf.Clamp(Mouse.current.position.ReadValue().y,  0 + shardSprite.rectTransform.sizeDelta.y /4 ,  Screen.height  -  shardSprite.rectTransform.sizeDelta.y /4));
+                transform.position = new Vector2(Math.Clamp(Mouse.current.position.ReadValue().x, 0  + shardSprite.rectTransform.sizeDelta.x /2,  mainCamera.pixelWidth - shardSprite.rectTransform.sizeDelta.x /2),
+                    Mathf.Clamp(Mouse.current.position.ReadValue().y,  0 + shardSprite.rectTransform.sizeDelta.y /2 ,  mainCamera.pixelHeight  -  shardSprite.rectTransform.sizeDelta.y /2));
             }
         }
         
