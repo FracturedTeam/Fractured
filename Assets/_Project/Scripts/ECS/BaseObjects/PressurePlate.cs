88193d81608a216c19504c31433f4740c967122f
LeBud <c.huyghe@rubika-edu.com>
2025-11-13 17:16:31 +0100

    Pressure plate

--- Assets/_Project/Scripts/ECS/BaseObjects/PressurePlate.cs
+++ Assets/_Project/Scripts/ECS/BaseObjects/PressurePlate.cs
@@ -1,9 +1,13 @@
 using System;
+using _Project.Scripts.Enums;
+using _Project.Scripts.Interfaces;
 using UnityEngine;
 
 namespace _Project.Scripts.ECS.BaseObjects {
-    public class PressurePlate : MonoBehaviour {
-
+    [RequireComponent(typeof(BaseObject))]
+    public class PressurePlate : MonoBehaviour , IInteractable {
+        private BaseObject baseObject;
+        
         [Header("Pressure Plate Settings")]
         [SerializeField] private Transform pressurePlateTriggerPos;
         [SerializeField] private Vector3 pressurePlateSize;
@@ -19,18 +23,32 @@ namespace _Project.Scripts.ECS.BaseObjects {
         private float timer;
         private readonly Collider[] results = new Collider[10];
 
-        private void Start() {
+        private bool initialized = false;
+
+        public void Initialize() {
+
+            if (!initialized) {
+                if(TryGetComponent(out BaseObject b)) baseObject = b;
+                else Debug.LogError($"[DoorInteractable] Cannot find {nameof(BaseObject)} in {nameof(PressurePlate)}");
+                
+                gameObject.layer = LayerMask.NameToLayer("Walkable");
+            }
+            
+            initialized = true;
             objectMoved.position = initialPos.position;
         }
 
-        void Update() {
+        public void OnInteract(ObjectInteraction interaction, IInteractable other = null) {
+        }
+
+        public void Tick(float deltaTime) {
             var size = Physics.OverlapBoxNonAlloc(pressurePlateTriggerPos.position, pressurePlateSize, results, transform.rotation, interactLayerMask );
             
             if(size >  0) {
-                timer += Time.deltaTime;
+                timer += deltaTime;
             }
             else {
-                timer -= Time.deltaTime;
+                timer -= deltaTime;
             }
             
             timer = Mathf.Clamp(timer, 0, timeToMoveObject);
@@ -38,6 +56,23 @@ namespace _Project.Scripts.ECS.BaseObjects {
             objectMoved.position = Vector3.Lerp(initialPos.position, movedPos.position, lerpValue);
             lerpValue = timer / timeToMoveObject;
         }
-        
+
+        public void ResetObject() {
+        }
+
+        public BaseObject GetBaseObject() {
+            return baseObject;
+        }
+
+        private void OnDrawGizmos() {
+            Gizmos.color = Color.cadetBlue;
+            Gizmos.DrawWireCube(pressurePlateTriggerPos.position, pressurePlateSize);
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(initialPos.position, 0.5f);
+            
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(movedPos.position, 0.5f);
+        }
     }
 }
