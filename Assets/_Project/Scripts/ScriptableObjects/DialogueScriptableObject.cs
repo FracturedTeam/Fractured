8ecae357089cabe05a9386f647e0df0ad8d280da
IdkHowButHereIAm <114149335+IdkHowButHereIAm@users.noreply.github.com>
2025-11-14 13:07:21 +0100

    passage en scriptable object

---
+++ Assets/_Project/Scripts/ScriptableObjects/DialogueScriptableObject.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace _Project.Scripts.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "Dialogue", menuName = "ScriptableObjects/DialogueScriptableObject")]
+    public class DialogueScriptableObject : ScriptableObject
+    {
+        public string dialogue;
+        
+        public string variableName;
+        public string basic;
+        public string red;
+        public string blue;
+        public string both;
+        
+        public int letters;
+    }
+}
