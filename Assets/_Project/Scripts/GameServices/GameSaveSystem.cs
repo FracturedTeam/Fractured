9c8debc543d2b876201aa76bee8c72c018c31b33
LeBud <c.huyghe@rubika-edu.com>
2025-12-02 17:12:02 +0100

    SaveSystemProgress

--- Assets/_Project/Scripts/GameServices/GameSaveSystem.cs
+++ Assets/_Project/Scripts/GameServices/GameSaveSystem.cs
@@ -1,39 +1,65 @@
 using System;
 using System.Collections.Generic;
 using _Project.Scripts.Enums;
+using _Project.Scripts.Systems.Save;
 using _Project.Scripts.Systems.Singletons;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace _Project.Scripts.GameServices {
 
     [Serializable]
     public struct GameData {
         public string Name;
+        public string CurrentLevelName;
+    }
+
+    public interface ISaveable {
+        string Id { get; set; }
+    }
+
+    public interface IBind<TData> where TData : ISaveable {
+        string Id { get; set; }
+        void Bind(TData data);
     }
     
     public class GameSaveSystem : Singleton<GameSaveSystem> {
         [SerializeField] public GameData gameData;
+
+        private IDataService dataService;
+
+        protected override void Awake() {
+            base.Awake();
+            dataService = new FileDataService(new JsonSerializer());
+        }
+
+        public void NewGame() {
+            gameData = new GameData {
+                Name = "New Game",
+                CurrentLevelName = "S_InteractableTesting"
+            };
+
+            SceneManager.LoadScene(gameData.CurrentLevelName, LoadSceneMode.Additive);
+        }
+
+        public void SaveGame() {
+            dataService.Save(gameData);
+        }
+
+        public void LoadGame(string gameName) {
+            gameData = dataService.Load(gameName);
+
+            if (String.IsNullOrWhiteSpace(gameData.CurrentLevelName)) {
+                gameData.CurrentLevelName = "S_InteractableTesting";
+            }
+            
+            SceneManager.LoadScene(gameData.CurrentLevelName, LoadSceneMode.Additive);
+        }
+
+        public void ReloadGame() => LoadGame(gameData.Name);
         
-    }
-
-    public struct SaveFile {
-        public string fileName;
-        public int currentScene;
-
-        public Vector3 playerPosition;
-
-        public List<SceneSaveFile> sceneFile;
-    }
-
-    [Serializable]
-    public struct SceneSaveFile {
-        public int buildIndex;
-        public ObjectsSaveFile[] objectsSave;
-    }
-
-    [Serializable]
-    public struct ObjectsSaveFile {
-        public Vector3 position;
-        public InteractionCompletion completion;
+        public void DeleteGame(string gameName) {
+            dataService.Delete(gameName);
+        }
     }
 }
