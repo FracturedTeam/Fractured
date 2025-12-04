be066887318b419b0f54c5facef0f68ffaeaf902
LeBud <c.huyghe@rubika-edu.com>
2025-12-04 16:37:32 +0100

    Zone editable

--- Assets/_Project/Scripts/GameServices/Services/ShardService.cs
+++ Assets/_Project/Scripts/GameServices/Services/ShardService.cs
@@ -16,6 +16,8 @@ namespace _Project.Scripts.GameServices.Services {
         private readonly List<BaseObject> shardsInteractable = new List<BaseObject>();
 
         public bool PlayerInEditableArea {get; private set;}
+        public bool PlayerInRedEditableArea {get; private set;}
+        public bool PlayerInBlueEditableArea {get; private set;}
         
         public void Initialize() { //Initialize the service
             interactables = new List<BaseObject>();
@@ -121,6 +123,14 @@ namespace _Project.Scripts.GameServices.Services {
             PlayerInEditableArea = inArea;
         }
         
+        public void SetRedEditableArea(bool inArea) {
+            PlayerInRedEditableArea = inArea;
+        }
+
+        public void SetBlueEditableArea(bool inArea) {
+            PlayerInBlueEditableArea = inArea;
+        }
+
         public void Dispose() {
             shardsInteractable.Clear();
         }
