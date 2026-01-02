b3614e5917e861dedbde63877cdcdc3036694045
lebud <c.huyghe@rubika-edu.com>
2026-01-02 14:51:42 +0100

    Anim V1

--- Assets/_Project/Scripts/Player/States/SubStates/GrabObjectState.cs
+++ Assets/_Project/Scripts/Player/States/SubStates/GrabObjectState.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace _Project.Scripts.Player.States.SubStates {
     public class GrabObjectState : PlayerBaseState{
-        private readonly CountdownTimer animationExitTimer;
+        public readonly CountdownTimer animationExitTimer;
 
         public GrabObjectState(PlayerController player, Animator animator, AnimationClip clip) : base(player, animator) {
             animationExitTimer = new CountdownTimer(clip.length);
