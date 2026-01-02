e206045d663333ee51430e55eeee4d185535dbd0
lebud <c.huyghe@rubika-edu.com>
2026-01-02 14:51:42 +0100

    Anim V1

--- Assets/_Project/Scripts/Player/States/SubStates/DropObjectState.cs
+++ Assets/_Project/Scripts/Player/States/SubStates/DropObjectState.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace _Project.Scripts.Player.States.SubStates {
     public class DropObjectState : PlayerBaseState {
-        private readonly CountdownTimer animationExitTimer;
+        public readonly CountdownTimer animationExitTimer;
 
         public DropObjectState(PlayerController player, Animator animator, AnimationClip clip) : base(player, animator) {
             animationExitTimer = new CountdownTimer(clip.length);
