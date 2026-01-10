1e7e390d6288d81feedb6737db022d182a673761
IdkHowButHereIAm <114149335+IdkHowButHereIAm@users.noreply.github.com>
2026-01-10 16:05:41 +0100

    setUpUI

--- Assets/_Project/Scripts/UI/ButtonUI.cs
+++ Assets/_Project/Scripts/UI/ButtonUI.cs
@@ -1,25 +1,38 @@
 using System;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace _Project.Scripts.UI
 {
     public class ButtonUI : MonoBehaviour
     {
+        [SerializeField] private Sprite baseSprite;
+        [SerializeField] private Sprite hoverSprite;
+        [SerializeField] private Sprite clickedSprite;
         private Vector3 scale;
         [SerializeField] private float time = 0.5f;
         [SerializeField] private  float multiplicator = 1.15f;
         
         Tweener tweener;
+        private Image backgroundImage;
         
         private void Awake()
         {
             scale = transform.localScale;
+            backgroundImage = GetComponent<Image>();
         }
 
         public void OnHover(bool hovering)
         {
-            tweener = transform.DOScale(hovering ? scale * multiplicator : scale, time).SetUpdate(true);
+            //tweener = transform.DOScale(hovering ? scale * multiplicator : scale, time).SetUpdate(true);
+            backgroundImage.sprite = hoverSprite;
+            
+        }
+        public void OnClicked()
+        {
+            //tweener = transform.DOScale(hovering ? scale * multiplicator : scale, time).SetUpdate(true);
+            backgroundImage.sprite = clickedSprite;
         }
 
         private void OnDestroy() {
