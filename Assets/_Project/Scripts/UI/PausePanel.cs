ca31655481d094c02d11aef7360e92eddaf3c6ad
IdkHowButHereIAm <114149335+IdkHowButHereIAm@users.noreply.github.com>
2026-01-18 02:54:46 +0100

    Move to namespace

--- Assets/_Project/Scripts/UI/PausePanel.cs
+++ Assets/_Project/Scripts/UI/PausePanel.cs
@@ -3,53 +3,56 @@ using _Project.Scripts.GameServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PausePanel : MonoBehaviour
+namespace _Project.Scripts.UI
 {
-    [SerializeField] private InputAction menuAction;
-    [SerializeField] private GameObject menu;
-    [SerializeField] private GameObject mainMenuPanel;
-    [SerializeField] private List<GameObject> panels;
-    private bool gameIsPaused;
-
-    private void Awake()
+    public class PausePanel : MonoBehaviour
     {
-        menuAction.performed += OnEscapePressed;
-        ReSet();
-    }
+        [SerializeField] private InputAction menuAction;
+        [SerializeField] private GameObject menu;
+        [SerializeField] private GameObject mainMenuPanel;
+        [SerializeField] private List<GameObject> panels;
+        private bool gameIsPaused;
 
-    private void OnEnable()
-    {
-        menuAction.Enable();
-    }
+        private void Awake()
+        {
+            menuAction.performed += OnEscapePressed;
+            ReSet();
+        }
 
-    private void OnDisable()
-    {
-        menuAction.Disable();
-    }
+        private void OnEnable()
+        {
+            menuAction.Enable();
+        }
 
-    private void OnEscapePressed(InputAction.CallbackContext context)
-    {
-        ChangeState();
-    }
+        private void OnDisable()
+        {
+            menuAction.Disable();
+        }
 
-    public void ChangeState()
-    {
-        gameIsPaused = !gameIsPaused;
-        Time.timeScale = gameIsPaused ? 0 : 1;
-        menu.SetActive(gameIsPaused);
-        ReSet();
-    }
+        private void OnEscapePressed(InputAction.CallbackContext context)
+        {
+            ChangeState();
+        }
 
-    private void ReSet()
-    {
-        mainMenuPanel.SetActive(gameIsPaused);
-        foreach (var panel in panels)
-            panel.SetActive(false);
-    }
+        public void ChangeState()
+        {
+            gameIsPaused = !gameIsPaused;
+            Time.timeScale = gameIsPaused ? 0 : 1;
+            menu.SetActive(gameIsPaused);
+            ReSet();
+        }
 
-    public void LoadMenu() {
-        Time.timeScale = 1;
-        GameSceneLoaderSystem.Instance.LoadMenu();
-    }
+        private void ReSet()
+        {
+            mainMenuPanel.SetActive(gameIsPaused);
+            foreach (var panel in panels)
+                panel.SetActive(false);
+        }
+
+        public void LoadMenu() {
+            Time.timeScale = 1;
+            GameSceneLoaderSystem.Instance.LoadMenu();
+        }
         
+    }
 }
