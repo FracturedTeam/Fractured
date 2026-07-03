134df848bc723a7154353a726b13069005c4c629
IdkHowButHereIAm <114149335+IdkHowButHereIAm@users.noreply.github.com>
2026-07-03 11:07:59 +0200

    Revert "newText"
    This reverts commit c66e62ab1068d7658cf64ed121d9e53cc4b7cd33.

--- Assets/_Project/Scripts/ScriptableObjects/GlassDocumentScriptableObject.cs
+++
@@ -1,8 +0,0 @@
-using _Project.Scripts.ScriptableObjects;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Glass Document", menuName = "ScriptableObjects/GlassDoc")]
-public class GlassDocumentScriptableObject : GlassTextScriptableObject
-{
-    public Sprite sprite;
-}
