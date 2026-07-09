62bc4040040dd03b6660df8a535406c5325d5a4f
IdkHowButHereIAm <114149335+IdkHowButHereIAm@users.noreply.github.com>
2026-07-09 17:37:11 +0200

    Editor

--- Assets/_Project/Scripts/ECS/TriggerComponent.cs
+++ Assets/_Project/Scripts/ECS/TriggerComponent.cs
@@ -1,3 +1,5 @@
+using System;
+using _Project.Scripts.ECS.BaseObjects;
 using _Project.Scripts.Enums;
 using UnityEditor;
 using UnityEngine;
@@ -6,85 +8,46 @@ using UnityEngine.UI;
 
 public class TriggerComponent : MonoBehaviour
 {
-    [HideInInspector] // HideInInspector makes sure the default inspector won't show these fields.
-    public bool StartTemp;
-
-    [HideInInspector]
-    public InputField iField;
-
-    [HideInInspector]
-    public GameObject Template;
-
-    // ... Update(), Awake(), etc
+    public bool showHideList = false; 
+  // [ShowIf(ActionOnConditionFail.JustDisable, ConditionOperator.And, nameof(showHideList))]
+    [SerializeField] private UnityEvent OnInteract;
+    
+    [SerializeField, ] internal UnityEvent OnCollisionTriggerEnter;
+    [SerializeField, ] private UnityEvent OnCollisionTriggerExit;
+    
+    [SerializeField, ] private UnityEvent OnHideReveal;
+    
+    [SerializeField, ] private UnityEvent OnTextHideReveal;
+    
+    [SerializeField, ] internal UnityEvent OnInteractSuccess;
+    [SerializeField, ] private UnityEvent OnInteractFailed;
+    
+    [SerializeField, ] internal UnityEvent OnSetStateOn;
+    [SerializeField, ] private UnityEvent OnSetStateOff;    
+    
+    
+    [SerializeField, ] internal UnityEvent OnSceneComplete;
+    [SerializeField, ] private UnityEvent OnAtelierComplete;
+    
 }
 
 #if UNITY_EDITOR
-[CustomEditor(typeof(TriggerComponent))]
-public class TriggerComponent_Editor : Editor
+public enum ConditionOperator
 {
-    
-    public override void OnInspectorGUI()
-    {
-        DrawDefaultInspector(); // for other non-HideInInspector fields
-
-        TriggerComponent script = (TriggerComponent)target;
-
-        // draw checkbox for the bool
-        script.StartTemp = EditorGUILayout.Toggle("Start Temp", script.StartTemp);
-        if (script.StartTemp) // if bool is true, show other fields
-        {
-            script.iField = EditorGUILayout.ObjectField("I Field", script.iField, typeof(InputField), true) as InputField;
-            script.Template = EditorGUILayout.ObjectField("Template", script.Template, typeof(GameObject), true) as GameObject;
-        }
-
-        if (GUILayout.Button("Your ButtonText"))
-        {
-        }
-    }
+    // A field is visible/enabled only if all conditions are true.
+    And,
+    // A field is visible/enabled if at least ONE condition is true.
+    Or,
 }
 
-public class EditorGUILayoutEnumPopup : EditorWindow
+public enum ActionOnConditionFail
 {
-    public TutorialTriggerType op;
-    [MenuItem("Examples/Editor GUILayout Enum Popup usage")]
-    static void Init()
-    {
-        UnityEditor.EditorWindow window = GetWindow(typeof(EditorGUILayoutEnumPopup));
-        window.Show();
-    }
-
-    void OnGUI()
-    {
-        op = (TutorialTriggerType)EditorGUILayout.EnumPopup("Primitive to create:", op);
-        if (GUILayout.Button("Create"))
-            InstantiatePrimitive(op);
-        UnityEditor.EditorWindow window = GetWindow(typeof(EditorGUILayoutEnumPopup));
-        window.Show();
-    }
-
-    void Open()
-    {
-        
-    }
-
-    void InstantiatePrimitive(TutorialTriggerType op)
-    {
-        switch (op)
-        {
-            case TutorialTriggerType.None:
-            case TutorialTriggerType.OnSuccess:
-            case TutorialTriggerType.OnInteract:
-            case TutorialTriggerType.OnHideReveal:
-            case TutorialTriggerType.OnLeavingMemory:
-            case TutorialTriggerType.OnUnsolved:
-            case TutorialTriggerType.OnCanBeSeen:
-            default:
-                Debug.LogError("Unrecognized Option");
-                break;
-        }
-    }
-    
-    
+    // If condition(s) are false, don't draw the field at all.
+    DontDraw,
+    // If condition(s) are false, just set the field as disabled.
+    JustDisable,
 }
+
+
 
 #endif
