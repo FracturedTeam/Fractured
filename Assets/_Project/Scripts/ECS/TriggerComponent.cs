963fffd8b802234a069c44807c5f268282c9be6e
IdkHowButHereIAm <114149335+IdkHowButHereIAm@users.noreply.github.com>
2026-07-10 15:03:57 +0200

    trigger

--- Assets/_Project/Scripts/ECS/TriggerComponent.cs
+++ Assets/_Project/Scripts/ECS/TriggerComponent.cs
@@ -6,48 +6,25 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(BaseObject))] 
 public class TriggerComponent : MonoBehaviour
 {
-    public bool showHideList = false; 
-  // [ShowIf(ActionOnConditionFail.JustDisable, ConditionOperator.And, nameof(showHideList))]
-    [SerializeField] private UnityEvent OnInteract;
+    [SerializeField] internal UnityEvent OnInteract;
     
     [SerializeField, ] internal UnityEvent OnCollisionTriggerEnter;
-    [SerializeField, ] private UnityEvent OnCollisionTriggerExit;
+    [SerializeField, ] internal UnityEvent OnCollisionTriggerExit;
     
-    [SerializeField, ] private UnityEvent OnHideReveal;
+    [SerializeField, ] internal UnityEvent OnHideReveal;
     
-    [SerializeField, ] private UnityEvent OnTextHideReveal;
+    [SerializeField, ] internal UnityEvent OnTextHideReveal;
     
     [SerializeField, ] internal UnityEvent OnInteractSuccess;
-    [SerializeField, ] private UnityEvent OnInteractFailed;
+    [SerializeField, ] internal UnityEvent OnInteractFailed;
     
     [SerializeField, ] internal UnityEvent OnSetStateOn;
-    [SerializeField, ] private UnityEvent OnSetStateOff;    
-    
+    [SerializeField, ] internal UnityEvent OnSetStateOff;    
     
     [SerializeField, ] internal UnityEvent OnSceneComplete;
-    [SerializeField, ] private UnityEvent OnAtelierComplete;
+    [SerializeField, ] internal UnityEvent OnAtelierComplete;
     
 }
-
-#if UNITY_EDITOR
-public enum ConditionOperator
-{
-    // A field is visible/enabled only if all conditions are true.
-    And,
-    // A field is visible/enabled if at least ONE condition is true.
-    Or,
-}
-
-public enum ActionOnConditionFail
-{
-    // If condition(s) are false, don't draw the field at all.
-    DontDraw,
-    // If condition(s) are false, just set the field as disabled.
-    JustDisable,
-}
-
-
-
-#endif
