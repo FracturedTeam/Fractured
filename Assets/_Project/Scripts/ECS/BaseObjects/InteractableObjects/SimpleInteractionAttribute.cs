a38e539641370e1051f91b1b77213f12fe5c9da6
IdkHowButHereIAm <114149335+IdkHowButHereIAm@users.noreply.github.com>
2026-07-13 13:01:31 +0200

    end of new interaction setup

--- Assets/_Project/Scripts/ECS/BaseObjects/InteractableObjects/SimpleInteractionAttribute.cs
+++ Assets/_Project/Scripts/ECS/BaseObjects/InteractableObjects/SimpleInteractionAttribute.cs
@@ -6,16 +6,15 @@ using UnityEngine;
 
 namespace _Project.Scripts.ECS.BaseObjects.InteractableObjects {
     [RequireComponent(typeof(BaseObject))]
-    public class SwitchStateAttribute : MonoBehaviour, IInteractable {
+    public class SimpleInteractionAttribute : MonoBehaviour, IInteractable {
         private BaseObject baseObject;
 
         private bool isInitialized;
-        [SerializeField] private bool stateOn = true;
         
         public void Initialize() {
             if (!isInitialized) {
                 if(TryGetComponent(out BaseObject component)) baseObject = component;
-                else throw new ArgumentNullException($"[MoveableObject] Cannot find {nameof(BaseObject)} in {nameof(MovableAttribute)}");
+                else throw new ArgumentNullException($"[MoveableObject] Cannot find {nameof(BaseObject)}");
 
                 baseObject.GetObjectType = ObjectType.None;
                 
@@ -27,17 +26,8 @@ namespace _Project.Scripts.ECS.BaseObjects.InteractableObjects {
 
         public void OnInteract(ObjectInteraction interaction, IInteractable other = null)
         {
-            stateOn = !stateOn;
-            ForceChangeState(stateOn);
+           
         }
-
-        //In case the GDs want to switch value via another object for some reason
-        public void ForceChangeState(bool isOn)
-        {
-            stateOn = isOn;
-            baseObject.GetTrigger?.OnFunction(stateOn ? baseObject.GetTrigger.OnSetStateOn : baseObject.GetTrigger.OnSetStateOff);
-        }
-        
  
 
         public void Tick(float deltaTime) {
