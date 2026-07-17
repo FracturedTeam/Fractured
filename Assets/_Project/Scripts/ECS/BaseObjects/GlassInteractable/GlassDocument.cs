2bf03835cd8953d13eb81b236d6df2d9d36f02cc
IdkHowButHereIAm <114149335+IdkHowButHereIAm@users.noreply.github.com>
2026-07-17 14:35:07 +0200

    template

--- Assets/_Project/Scripts/ECS/BaseObjects/GlassInteractable/GlassDocument.cs
+++ Assets/_Project/Scripts/ECS/BaseObjects/GlassInteractable/GlassDocument.cs
@@ -1,22 +1,22 @@
+using System.Collections.Generic;
 using _Project.Scripts.Player;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GlassDocument : MonoBehaviour
 {
-    [SerializeField] private Image background;
-    [SerializeField] private GlassText glassText;
     [SerializeField] private float distance;
+    [SerializeField] private List<GlassDocumentTemplate> templates;
+    
     private Transform cameraTrans;
     
-    public void SetUp(GlassDocumentScriptableObject data)
+    public void SetUp(GlassDocumentScriptableObject data, bool isOn = true)
     {
         cameraTrans = PlayerController.Instance.cinemachineBrain.OutputCamera.transform;
-        glassText.Setup(data);
-        glassText.Appear();
         transform.position = cameraTrans.position + cameraTrans.forward * distance;
         transform.LookAt(cameraTrans);
         transform.eulerAngles = new Vector3(20, 180 + transform.eulerAngles.y, 0);
-        //background.sprite = data.sprite;
+        templates[(int)data.type].gameObject.SetActive(isOn);
+        templates[(int)data.type].SetUp(data);
     }
 }
