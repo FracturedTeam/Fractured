386ea719cf462f1869fa67acc9d149bfc748af82
IdkHowButHereIAm <114149335+IdkHowButHereIAm@users.noreply.github.com>
2026-07-23 12:40:19 +0200

    test

--- Assets/_Project/Scripts/Editor/EnviroColorTool.cs
+++ Assets/_Project/Scripts/Editor/EnviroColorTool.cs
@@ -132,27 +132,5 @@ public class EnviroColorTool : EditorWindow
         }
     }
     
-    public class Profil : ScriptableObject
-    {
-        public  int act = 1;
-        public  float transition = 0;
-
-        public  Color act1_Color_A;
-        public  Vector2 act1_Color_A_Location = new Vector2(0f,0.5f);
-        public  Color act1_Color_B;
-        public  Vector2 act1_Color_B_Location = new Vector2(0.5f, 1f);
-        public  Color act1_Color_C;
-
-        public  Color act2_Color_A;
-        public  Vector2 act2_Color_A_Location = new Vector2(0f, 0.5f);
-        public  Color act2_Color_B;
-        public  Vector2 act2_Color_B_Location = new Vector2(0.5f, 1f);
-        public  Color act2_Color_C;
-
-        public  Color act3_Color_A;
-        public  Vector2 act3_Color_A_Location = new Vector2(0f, 0.5f);
-        public  Color act3_Color_B;
-        public  Vector2 act3_Color_B_Location = new Vector2(0.5f, 1f);
-        public  Color act3_Color_C;
-    }
+   
 }
