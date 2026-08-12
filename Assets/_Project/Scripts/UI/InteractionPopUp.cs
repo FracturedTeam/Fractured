147d7d84714e52836a8b8d15207d8cfa544191fb
lebud <c.huyghe@rubika-edu.com>
2026-08-12 12:55:50 +0200

    UI Update

--- Assets/_Project/Scripts/UI/InteractionPopUp.cs
+++ Assets/_Project/Scripts/UI/InteractionPopUp.cs
@@ -1,4 +1,3 @@
-using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,27 +7,18 @@ namespace _Project.Scripts.UI
     public class InteractionPopUp : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI interactionText;
-        [SerializeField] private Image interactionImage;
         public CanvasGroup GetGroup { get; private set; }
-        public Image GetInteractionImage { get; private set; }
         public TextMeshProUGUI GetInteractionText { get; private set; }
         
         private void Awake()
         {
             GetGroup = GetComponent<CanvasGroup>();
             GetInteractionText = interactionText;
-            GetInteractionImage =  interactionImage;
         }
 
         public void SetData(string text)
         {
             interactionText.text = text;
         }
-        
-        public void SetData(Sprite image)
-        {
-            interactionImage.sprite = image;
-        }
-
     }
 }
